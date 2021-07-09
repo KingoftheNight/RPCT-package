e7c1b85886cc94cdcc8c8de53ecf374f4221e80b
Liang YuChao <43328497+KingoftheNight@users.noreply.github.com>
2021-07-09 11:08:53 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="rpct",
+    version="1.0",
+    author="Liang YC",
+    author_email="1694822092@qq.com",
+    description="A RAAC-PSSM-based Protein Prediction Toolkit",
+    long_description="none",
+    long_description_content_type="text/markdown",
+    url="https://github.com/KingoftheNight/RPCT-package",
+    packages=["pyrpct"],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scikit-learn',
+        'seaborn',
+        'pyecharts'
+        ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    zip_safe=True)
